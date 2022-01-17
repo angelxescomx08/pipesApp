cd25ab3f467ac7793448de26f72daaae59f8efca
ahxdevelopmx <ah@xdevelop.mx>
2022-01-17 16:42:21 -0600

    fin de la sección

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -9,9 +10,42 @@ import { Component } from '@angular/core';
 export class OrdenarComponent{
 
   enMayusculas : boolean = true;
+  ordenarPor: string = '';
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Spiderman',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna verde',
+      vuela: true,
+      color: Color.verde
+    },
+  ];
 
   cambiar(){
     this.enMayusculas = !this.enMayusculas;
+  }
+
+  cambiarOrden(orden: string){
+    this.ordenarPor = orden;
   }
 
 }
